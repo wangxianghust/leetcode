c9e2c29b1b4a158b6331ee80d566f2e8402d3b3e
wangxianghust <xianghust@gmail.com>
2017-03-07 09:39:57 +0800

    update problem2
    you need to delete the debug info when you run it on the leetcode
    platform.

--- 2AddTwo.cpp
+++ 2AddTwo.cpp
@@ -14,56 +14,57 @@ ListNode* AddTwo(ListNode* l1, ListNode *l2){
     vector<int> l;
     vector<int> r;
     vector<int> ret;
-    ListNode *retL(nullptr);
-    while(l1 != nullptr){
+    while(l1){
         l.push_back(l1->val);
+        cout << "l1 : " << l1->val << endl;
         l1 = l1->next;
     }
-    while(l2 != nullptr){
+    while(l2){
         r.push_back(l2->val);
+        cout << "l2 : " << l2->val << endl;
         l2 = l2->next;
     }
-    int l1_size = l.size();
-    int l2_size = r.size();
 
-    reverse(l.begin(), l.end());
-    reverse(r.begin(), r.end());
-    if(l1_size < l2_size) swap(l, r);
+    if(l.size() < r.size()) swap(l, r);
+    for(auto i:l)cout << "l " << i << endl;
+    for(auto i:r)cout << "r " << i << endl;
 
-        int c = 0;
-        for(int i=0; i<l2_size; ++i){
-            int tmp = (l[i] + r[i] + c)%10;
-            c = (l[i] + r[i])/10;
-            ret.push_back(tmp);
-        }
-        for(int i=l2_size; i<l1_size; ++i){
-            int tmp = (l[i] + c)%10;
-            c = (l[i] + c) / 10;
-            ret.push_back(tmp);
-        }
-        if(c==1)ret.push_back(c);
-        
-    for(int i=0; i<ret.size(); ++i){
-        ListNode *tmp = new ListNode(ret[i]);
-        retL = tmp;
-        retL = retL->next;
+	int c = 0;
+	for(int i=0; i<r.size(); ++i){
+	    int tmp = (l[i] + r[i] + c)%10;
+	    c = (l[i] + r[i] + c)/10;
+	    ret.push_back(tmp);
+	}
+	for(int i=r.size(); i<l.size(); ++i){
+	    int tmp = (l[i] + c)%10;
+	    c = (l[i] + c) / 10;
+	    ret.push_back(tmp);     
+	}
+    if(c==1)ret.push_back(c);
+
+    //reverse(ret.begin(), ret.end());
+    
+    for(auto i:ret) cout << "vec " << i << endl;
+    ListNode *retL = new ListNode(ret[0]); 
+    ListNode *root = retL;
+    for(int i=1; i<ret.size(); ++i){
+        root->next = new ListNode(ret[i]);
+        root = root->next;
     }
+    //root->next = nullptr;
     return retL;
 }
 
 int main(){
-    ListNode *a = new ListNode(1);
-    ListNode *b = new ListNode(2);
-
-    ListNode* l1(nullptr);
-    l1->next = a;
-    l1->next = b;
-    ListNode* l2(nullptr);
-    l2->next = b;
-    l2->next = a;
+    ListNode* l1 = new ListNode(3);
+    l1->next = new ListNode(7);
+    //l1->next->next = new ListNode(9);
+    ListNode* l2 = new ListNode(9);
+    l2->next = new ListNode(2);
     
-    auto ret = AddTwo(l1, l2);
-    while(ret->next != nullptr){
-        cout << ret->val << endl;
+    auto ret = AddTwo(l2, l1);
+    while(ret){
+        cout << "ret " << ret->val << endl;
+        ret = ret->next;
     }
 }
