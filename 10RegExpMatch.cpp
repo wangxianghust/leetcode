c6b94447ad4a5f0b72187e8f992581e67539e898
wangxianghust <xianghust@gmail.com>
2017-03-15 21:26:31 +0800

    Pro 10
    update

--- 10RegExpMatch.cpp
+++ 10RegExpMatch.cpp
@@ -18,7 +18,7 @@ public:
                 book[0][j+1] = true;  //This is j+1 not j, p[j] have the length j.
             }
         }
-        for(int i=0; i<s_len; ++i){
+        for(int i=0; i<s_len; ++i){ //It is discussing the value of p[j]
             for(int j =0; j<p_len; ++j){
                 if(s[i] == p[j] || p[j] == '.'){
                     book[i+1][j+1] = book[i][j];
