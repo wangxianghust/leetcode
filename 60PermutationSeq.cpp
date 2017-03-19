f8b9d6defd8dc0462c75e4125d7f8e3c52d49d37
wangxianghust <xianghust@gmail.com>
2017-03-19 16:04:21 +0800

    Pro 16
    update logical

--- 60PermutationSeq.cpp
+++ 60PermutationSeq.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
 #include <string>
+#include <set>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
+    string getPermutation(int n, int k){
+        string ret(n, '0');
+        vector<int> factorial(10,1);
+        for(int i=1; i<10; ++i){ //factorial[i] = i!
+            factorial[i] = factorial[i-1] * i;
+        }
+        vector<char> nums; //123..9
+        for(int i=1; i<10; ++i){
+            nums.push_back('0'+i);
+        }
+        int i = 0;
+        while(n){
+            int index = (k-1)/factorial[n-1];//there must be k-1, cause we want to know the numbers before k.
+            ret[i++] = nums[index];
+            nums.erase(nums.begin() + index);
+            k = k - index*factorial[n-1];
+            --n;
+        }
+        return ret;
+    }
+public:
     //ERROR : Time Limit Exceeded
-    string getPermutation(int n, int k) {
+    string getPermutation_(int n, int k) {
        //permutation("", n, k);
        count =  k;
        permutation("", n);
@@ -46,5 +69,6 @@ int main(){
         int n,k;
         cin >> n >> k;
         cout << "answer " << Sol.getPermutation(n, k) << endl;
+        cout << "backtracking " << Sol.getPermutation_(n, k) << endl;
     }
 }
