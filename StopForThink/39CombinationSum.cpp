3d4bc1db147a5b4e9659fcdc58a59376f024fb5c
wangxianghust <xianghust@gmail.com>
2017-03-22 15:42:57 +0800

    Pro 39 backtracking
    Solve

---
+++ StopForThink/39CombinationSum.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+string indent = " ";
+
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int> > ret;
+        vector<int> combination;
+        combinationSum(candidates, target, combination, ret, 0);
+        return ret;
+    }
+private:
+    void combinationSum(vector<int> &candidates, int target, vector<int> &combination, vector<vector<int> > &ret, int begin){
+        if(target == 0){
+            ret.push_back(combination);
+            //for(auto i: combination) cout << i << "--"; cout << endl;
+            return;
+        }
+        for(int i=begin; i<candidates.size() && target >= candidates[i]; ++i){
+            enter(candidates, i);
+            combination.push_back(candidates[i]);
+            combinationSum(candidates, target-candidates[i], combination, ret, i);
+            combination.pop_back();
+            print(combination);
+        }
+    }
+private:
+    static bool debug;
+    void enter(vector<int> &candidates, int i){
+        if(debug){
+            cout << indent << "Entering " << candidates[i] << endl;
+            indent += "|  ";
+        }
+     }
+    void print(vector<int> &combinations){
+        if(debug){
+            indent = indent.substr(3);
+            cout << indent << "can : ";
+            for(auto i : combinations) cout << i << " ";
+            cout << endl;
+        }
+    }
+public:
+    void print(vector<vector<int>> &ret){
+        for(auto i : ret){
+            for(auto j : i)cout << j << " ";
+            cout << endl;
+        }
+        cout << "---" << endl;
+    }
+
+};
+
+bool Solution::debug = false;
+
+
+int main(){
+    freopen("39test.txt", "r", stdin);
+    int total;
+    cin >> total;
+    Solution Sol;
+    for(int i=0; i<total; ++i){
+        int target;
+        int size;
+        cin >> target >> size;
+        vector<int> can;
+        for(int i=0; i<size; ++i){
+            int c;
+            cin >> c;
+            can.push_back(c);
+        }
+        auto ret = Sol.combinationSum(can, target);
+        Sol.print(ret);
+    }
+}
--- StopForThink/backtracking.md
+++ StopForThink/backtracking.md
@@ -1,8 +1,8 @@
 ### BackTracking
 ***
-http://algorithms.tutorialhorizon.com/introduction-to-backtracking-programming/
-
+https://www.cis.upenn.edu/~matuszek/cit594-2012/Pages/backtracking.html
 **概念**
+---
 回溯在tree,graph上的实现是DFS，回溯可以使用递归实现（当然用stack）模拟生成也是可以的。
 
 **一般形式**
@@ -36,17 +36,20 @@ bool solve(Node n){
 }
 ```
 
-*Some interesting founds:*
-*stack algorithm terminates successfully, the nodes on stack form the path(reverse order) from root to goal leaf.
+Some interesting founds:
+--
+stack algorithm terminates successfully, the nodes on stack form the path(reverse order) from root to goal leaf.
 
-**How to make codes clean**
-*1 function isLeaf to decide the leaf
-*2 list all children, if the children have the ORDER is best.convience for determining the next try.
+How to make codes clean
+---
+- function isLeaf to decide the leaf
+- list all children, if the children have the ORDER is best.convience for determining the next try.
     上面在具体问题中并不直观，需要找到合理的遍历子节点的方式
-*3 use isSafe() funtion to cut some children when choose next child.
+- use isSafe() funtion to cut some children when choose next child.
 
 ***DUBUG***
--学会使用缩进，同时使用specialized print function. eg
+---
+学会使用缩进，同时使用specialized print function. eg
 ```cpp
 const bool debugging = false;
 void Enter(node){
@@ -55,4 +58,42 @@ void Enter(node){
         indent = indent + "| ";
     }
 }
+```
+
+Template
+------
+https://segmentfault.com/a/1190000006121957
+关于回溯问题，有三种问题
+- 返回值是true or false(如上的形式)
+- 求个数/所有解的信息 --> 设置全局变量，返回值是void
+- 设全局best，返回值是void
+```cpp
+// Get counter or results
+void solve(Node n){
+    if (n == leaf){
+        if(n is a goal){
+            ++count;
+            //result.push_back(n);
+        } else return;
+    } else {
+        for each child c of n{
+            solve(c);
+        }
+    }
+}
+
+//Get best
+void solve(Node n){
+    if(n == leaf){
+        if(n is a goal){
+            UPDATE best;
+            return;
+        } else return;
+    } else {
+        for each child c of n{
+            solve(c);
+        }
+    }
+}
+
 ```
