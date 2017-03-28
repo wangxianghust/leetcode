b0b7d99b178856fca035a2a50686a602df9550b7
wangxianghust <xianghust@gmail.com>
2017-03-28 19:52:57 +0800

    Pro 51
    Solve

--- 51NQueens.cpp
+++ 51NQueens.cpp
@@ -1,44 +1,58 @@
-/*求出NQueens的解*/
+/*求出NQueens的解
+ * 经典的回溯法就可以解决，稍微tricky的地方是，matrix不是用二维矩阵保存的，一维就可以
+ */
 #include <vector>
 #include <string>
 #include <iostream>
+#include <climits>
 
 using namespace std;
 
 class Solution {
 public:
-    vector<vector<int> > solveNQueens(int n) {
+    vector<vector<string> > solveNQueens(int n) {
         vector<int> matrix(n, -1);//every row have one queen, the value is the column index.
         vector<vector<int> > ret;
+        vector<vector<string> > output;
         solveNQueens(ret, matrix, 0);
-        return ret;
+        convert(ret, output, n);
+        return output;
     }
 
 private:
+    void convert(vector<vector<int> > &ret, vector<vector<string> > &output, int n){
+        for(auto item : ret){ //every item is a solution.
+            vector<string> lines;
+            for(int i=0; i<n; ++i){
+                string line(n, '.');
+                int column = item[i];
+                line[column] = 'Q';//put the Q
+                lines.push_back(line);
+            }
+            output.push_back(lines);
+        }
+    }
+
     void solveNQueens(vector<vector<int> > &ret, vector<int> &matrix, int row){
         int size = matrix.size();
-        if(row > size){
+        if(row == size){
            ret.push_back(matrix);
-           for(int &i:matrix) i = -1;
            return;
         }
-        for(int i=row; i<size; ++i){
-            for(int j=0; j<size; ++j){
-                if(isValid(matrix, i, j)){
-                    matrix[i] = j;
-                    solveNQueens(ret, matrix, row+1);
-                    matrix[i] = -1;
-                    //cout << matrix[i] << endl;
-                }
+        
+        for(int column=0; column<size; ++column){
+            if(isValid(matrix, row, column)){
+                matrix[row] = column;
+                solveNQueens(ret,  matrix, row+1);
+                matrix[row] = -1; 
             }
         }
     }
 
     bool isValid(vector<int> &matrix, int row, int column){
-        int size = matrix.size();
-        for(int i=0; i<size; ++i){
+        for(int i=0; i<row; ++i){
             int j = matrix[i]; //get the column of row i
-            if(abs(j-i) == abs(row-column) || column == j)  return false;
+            if(abs(row-i) == abs(column-j) || column == j)  return false;
         }
         return true;
     }
@@ -47,11 +61,11 @@ public:
         void print(vector<vector<T> > ret){
             for(auto item : ret){
                 for(T i : item){
-                    cout << i << " ";
+                    cout << i << endl;
                 }
-                cout << endl;
+                cout << "Another soluton" << endl;
             }
-            cout << "---" << endl;
+            cout << "---------" << endl;
         }
 };
 
