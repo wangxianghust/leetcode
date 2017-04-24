78fe8936f769ae19e2ede3e271594af694593df1
wangxianghust <xianghust@gmail.com>
2017-04-24 23:09:01 +0800

    update
    test

--- 105ConstructFromPreAndIn.cpp
+++ 105ConstructFromPreAndIn.cpp
@@ -3,4 +3,25 @@
  * 递归构建tree
  */
 
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x): val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        
+    }
+};
+
+int main(){
+    //AC
+}
