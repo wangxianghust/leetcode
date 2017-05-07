25c61d4d0b17c3db3b1af5a65774c50776ce530e
wangxianghust <xianghust@gmail.com>
2017-05-07 20:58:25 +0800

    Solve

---
+++ 107LevelTravel.cpp
@@ -0,0 +1,21 @@
+/* 二叉树层次遍历，从底向上
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        
+    }
+};
---
+++ 108Convert2BST.cpp
@@ -0,0 +1,39 @@
+/*给定一个升序的数组，求高度平衡的BST
+ * Solution:要求高度平衡，那么每次选择数组中间的元素作为root,剩下两边作为左右子树
+ * 遍历即可
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        int size = nums.size();
+        if(size == 0) return NULL;
+        return sortedArrayToBST(nums, 0, size-1);
+    }
+private:
+    TreeNode* sortedArrayToBST(vector<int> &nums, int start, int end){
+        TreeNode* root;
+        if(start > end) return NULL;
+        if(start == end) {
+            root = new TreeNode(nums[start]);
+            return root;
+        }
+        int mid = (end-start)/2 + start;
+        root = new TreeNode(nums[mid]);
+        root->left = sortedArrayToBST(nums, start, mid-1);
+        root->right = sortedArrayToBST(nums, mid+1, end);
+        return root;
+    }
+};
