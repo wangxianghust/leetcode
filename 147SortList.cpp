0c2270db44f868a166d4f37b4db53bc9706bc232
wangxianghust <xianghust@gmail.com>
2017-05-14 12:24:55 +0800

    List

---
+++ 147SortList.cpp
@@ -0,0 +1,19 @@
+/* 使用插入排序的方法排序一个list
+ * Solution:1.如果对内存没有要求，将value拷贝出来，排序后再放回去
+ * 2.对list纯操作的话，没有难度，设置一个dummy，然后每次寻找正确位置的时候都从dummy开始遍历
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        
+    }
+};
---
+++ 148SortList.cpp
@@ -0,0 +1,18 @@
+/* 使用O(n*lg n)排序一个List，常数space
+ * Solution： merge sort, 对list 取mid时候，使用slow 1 step，fast 2 step 的策略
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        
+    }
+};
