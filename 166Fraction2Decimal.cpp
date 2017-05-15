eeb87dd4b4537231edd1e951338142d4f0b36175
wangxianghust <xianghust@gmail.com>
2017-05-15 22:04:55 +0800

    Update

---
+++ 166Fraction2Decimal.cpp
@@ -0,0 +1,11 @@
+/*模拟出发操作，将分数变成小数，对于循环的部分使用()扩起来，如 1/3 = 0.(3), 2/5 = 0.4, -10/2 = -5
+ * Solution:先对符号，整数部分进行处理，然后模拟小数计算，使用hash_map记录除数的位置，一旦发现除数再次出现，则发现了循环体（患者除数
+ * 为0），这两个都是终止条件。
+ */
+
+class Solution {
+public:
+    string fractionToDecimal(int numerator, int denominator) {
+        
+    }
+};
---
+++ 167TwoSum.cpp
@@ -0,0 +1,10 @@
+/* 给定一个排序的数组，求两个数相加为target
+ * Solution：start,end指针，向中间夹逼法
+ */
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        
+    }
+};
