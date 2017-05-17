529e2cf9fbcabeb265c8a2b25b7f91fb5750ad55
wangxianghust <xianghust@gmail.com>
2017-05-17 21:48:28 +0800

    update

---
+++ 189RotateArray.cpp
@@ -0,0 +1,11 @@
+/* 将一个数组旋转，如1,2,3,4,5,6,7  k=3  5,6,7,1,2,3,4
+ * Solution : 1. 再开一个数组，旋转
+ * 2. 先旋转(n-k), k，再全部旋转
+ */
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        
+    }
+};
