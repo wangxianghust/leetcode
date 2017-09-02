9b8fc57b9594f355726a737bf71e9059f13c9cd4
wangxianghust <xianghust@gmail.com>
2017-09-02 15:48:44 +0800

    Solve

--- 112PathSum.cpp
+++ 112PathSum.cpp
@@ -1,7 +1,8 @@
 /* 判断二叉树是否有一个路径和是sum
  * 递归判断法，从root到child，每次sum = sum-root->val;
  */
-
+#include <iostream>
+using namespace std;
 struct TreeNode {
     int val;
     TreeNode *left;
@@ -12,6 +13,8 @@ struct TreeNode {
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int sum) {
-        
+        if(root == NULL) return false;
+        if(root->left == NULL && root->right == NULL && root->val == sum) return true;
+        return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
     }
 };
