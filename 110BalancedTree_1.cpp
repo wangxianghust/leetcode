2c1566781ea0c47deb7513dc096a128a9baf020a
wangxianghust <xianghust@gmail.com>
2017-09-02 15:35:36 +0800

    Solve

--- 110BalancedTree_1.cpp
+++ 110BalancedTree_1.cpp
@@ -1,7 +1,6 @@
 /* 判断一个Tree是不是balanced tree
  * Solution: 采用从底向上的方法，计算每个节点左右子树的高度，一旦发现高度差超过1，return -1，
  * 后面只要检测是否为-1，是的话继续返回-1，不需要向自上向下的方法那样来遍历每个节点计算
- * 这是bottom to up的方法。
  */
 
 #include <iostream>
@@ -20,19 +19,14 @@ struct TreeNode {
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
-        return getHeight(root) != -1;        
+        if(root == NULL) return true;
+        int l_height = getHeight(root->left);
+        int r_height = getHeight(root->right);
+        return (abs(l_height - r_height) < 2 && isBalanced(root->left) && isBalanced(root->right)) ? 1 : 0;
     }
 
     int getHeight(TreeNode* root){
-    //如果此时的节点balanced，返回height，or 返回 -1
         if(root == NULL) return 0;
-        int left = getHeight(root->left);
-        if(left == -1) return -1;
-        int right = getHeight(root->right);
-        if(right == -1) return -1;
-
-        if(abs(left - right) > 1) return -1;
-
         return 1 + max(getHeight(root->left), getHeight(root->right));
     }
 };
