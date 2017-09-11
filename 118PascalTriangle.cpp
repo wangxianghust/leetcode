6b74db2547c27b8567c14842e74cbb9cceea5eae
wangxianghust <xianghust@gmail.com>
2017-09-11 13:35:12 +0800

    AC

--- 118PascalTriangle.cpp
+++ 118PascalTriangle.cpp
@@ -11,6 +11,17 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        
+      if(numRows == 0) return vector<vector<int>>();
+      vector<vector<int>>  ret(numRows);
+      ret[0].push_back(1);
+      for(int i=1; i<numRows; ++i){
+          ret[i].resize(i+1);
+          ret[i][0] = 1;
+          ret[i][i] = 1;
+          for(int j=1; j<i; ++j){
+              ret[i][j] = ret[i-1][j-1] + ret[i-1][j];
+          }
+      }
+      return ret;
     }
 };
