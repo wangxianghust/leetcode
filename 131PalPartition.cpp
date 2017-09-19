df4341e4df4a77cc94107c182075aa139a811161
wangxianghust <xianghust@gmail.com>
2017-09-19 16:27:33 +0800

    Solve

--- 131PalPartition.cpp
+++ 131PalPartition.cpp
@@ -3,9 +3,39 @@
  * DP
  */
 
+#include <vector>
+#include <string>
+
+using namespace std;
+
 class Solution {
 public:
     vector<vector<string>> partition(string s) {
-        
+        vector<vector<string>> ret;
+        if(s.size() == 0) return ret;
+        vector<string> path;
+        helper(0, s, path, ret);
+        return ret;
+    }
+
+    void helper(int idx, string &s, vector<string> path, vector<vector<string> > &ret){
+        if(idx == s.size()){
+            ret.push_back(path);
+            return;
+        }
+        for(int i=idx; i<s.size(); ++i){
+            if(isPalindrome(s, idx, i)){
+                path.push_back(s.substr(idx, i-idx+1));
+                helper(i+1, s, path, ret);
+                path.pop_back();
+            }
+        }
+    }
+
+    bool isPalindrome(string &s, int start, int end){
+        while(start < end){
+            if(s[end--] != s[start++]) return false;
+        }
+        return true;
     }
 };
