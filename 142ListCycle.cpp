a3b49b24b24e60e93fb1c4122790df4483dfa039
wangxianghust <xianghust@gmail.com>
2017-09-28 11:04:17 +0800

    solved

--- 142ListCycle.cpp
+++ 142ListCycle.cpp
@@ -6,17 +6,54 @@
  * 还有更简单的，需要证明：假设相遇点是m,则从head和m点两个指针同时走，相遇点是entry
  */
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+#include <iostream>
+
+using namespace std;
+
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+ 
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        
+        ListNode* tmp = isCycle(head);
+        if(tmp == NULL) return NULL;
+        int len = getCycleLen(tmp);
+        return getStart(head, len);      
+    }
+
+private:
+    ListNode* isCycle(ListNode* head){
+        if(head == NULL || head->next == NULL) return NULL;
+        ListNode *slow(head), *fast(head);
+        while(fast->next && fast->next->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) return fast;
+        }
+        return NULL;
+    }
+
+    int getCycleLen(ListNode* head){
+        int ret = 1;
+        ListNode* phead(head->next);
+        while(head != phead){
+            ++ret;
+            phead = phead->next;
+        }
+        return ret;
+    }
+
+    ListNode* getStart(ListNode* head, int len){
+        ListNode *slow(head), *fast(head);
+        while(len--) fast = fast->next;
+        while(slow != fast){
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return fast;
     }
 };
--- 143ReorderList.cpp
+++ 143ReorderList.cpp
@@ -3,18 +3,57 @@
  * 2. reverse后半段，即变为 1->2->3 6->5->4
  * 3. merge 两个链表即可。
  */
+#include <iostream>
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+using namespace std;
+
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+ 
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        
+        if(head == NULL || head->next == NULL) return;
+        ListNode *p1 = head, *p2 = head->next;
+        while(p2 && p2->next){
+            p1 = p1->next;
+            p2 = p2->next->next;
+        }
+
+        ListNode *head2 = p1->next;
+        p1->next = NULL;
+
+        head2 = reverse(head2);
+        merge(head, head2);
+    }
+
+    ListNode* reverse(ListNode* head){
+        ListNode* prev(NULL);
+        ListNode* cur(head);
+        ListNode* ret;
+        while(cur){
+            ret = cur;
+            ListNode* next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return ret;
+    }
+
+    ListNode* merge(ListNode* l1, ListNode* l2){
+        ListNode* head(l1);
+        while(l1 && l2){
+            ListNode* pl1 = l1->next;
+            l1->next = l2;
+            ListNode* pl2 = l2->next;
+            l2->next = pl1;
+            l1 = pl1;
+            l2 = pl2;
+        }
+        return head;
     }
 };
